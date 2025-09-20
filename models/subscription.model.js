a151028757e52f91625c9b1d9b5571307d013a4a
jatisusan <susurrus112@gmail.com>
2025-09-20 18:57:03 +0545

    Implement create and get user subscription

--- models/subscription.model.js
+++ models/subscription.model.js
@@ -56,7 +56,6 @@ const subscriptionSchema = new mongoose.Schema(
     },
     renewalDate: {
       type: Date,
-      required: true,
       validate: {
         validator: function (value) {
           return value > this.startDate;
--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,18 +1,25 @@
 import { Router } from "express";
+import authorize from "../middlewares/auth.middleware.js";
+import {
+  createSubscription,
+  getUserSubscriptions,
+} from "../controllers/subscription.controller.js";
 
 const subscriptionRouter = Router();
 
-subscriptionRouter.get("/", (req, res) => {res.send({title: 'GET all subscriptions'})});
+subscriptionRouter.get("/", (req, res) => {
+  res.send({ title: "GET all subscriptions" });
+});
 
 subscriptionRouter.get("/:id", () => {});
 
-subscriptionRouter.post("/", () => {});
+subscriptionRouter.post("/", authorize, createSubscription);
 
 subscriptionRouter.put("/:id", () => {});
 
 subscriptionRouter.delete("/:id", () => {});
 
-subscriptionRouter.get("/user/:id", () => {});
+subscriptionRouter.get("/user/:id", authorize, getUserSubscriptions);
 
 subscriptionRouter.put("/:id/cancel", () => {});
 
