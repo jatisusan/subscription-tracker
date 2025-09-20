dc0c1d3cb15acf7a2fb27faa11ac10364fd338d9
jatisusan <susurrus112@gmail.com>
2025-09-20 18:26:15 +0545

    Implement bot projection and rate limit with arcjet

---
+++ middlewares/arcjet.middleware.js
@@ -0,0 +1,22 @@
+import aj from "../config/arcjet.js";
+
+const arcjetMiddleware = async (req, res, next) => {
+  try {
+    const decision = await aj.protect(req, { requested: 1 });
+
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit())
+        return res.status(429).json({ error: "Rate limit exceeded!" });
+      if (decision.reason.isBot())
+        res.status(403).json({ error: "Bot detected!" });
+      return res.status(403).json({ error: "Access denied!" });
+    }
+
+    next();
+  } catch (err) {
+    console.log(`Arcjet Middleware Error: ${err}`);
+    next(err);
+  }
+};
+
+export default arcjetMiddleware;
