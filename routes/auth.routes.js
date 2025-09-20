41631c90c23428c10f52c280017567383ae1b9b9
jatisusan <susurrus112@gmail.com>
2025-09-20 17:13:52 +0545

    Implement authentication(signin/up)

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
+import { signIn, signOut, signUp } from "../controllers/auth.controller.js";
 
 const authRouter = Router();
 
-authRouter.post("/sign-up", () => {});
+authRouter.post("/sign-up", signUp);
 
-authRouter.post("/sign-in", () => {});
+authRouter.post("/sign-in", signIn);
 
-authRouter.post("/sign-out", () => {});
+authRouter.post("/sign-out", signOut);
 
 export default authRouter;
