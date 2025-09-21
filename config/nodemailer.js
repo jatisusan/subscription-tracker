c1adf33cbaf3fc38fce76751c5fcbec6f4b6cbce
jatisusan <susurrus112@gmail.com>
2025-09-21 17:36:45 +0545

    Fix nodemailer error

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -2,9 +2,7 @@ import nodemailer from "nodemailer";
 import { EMAIL_ADDRESS, EMAIL_PASSWORD } from "./env.js";
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.ethereal.email",
   service: "gmail",
-  port: 587,
   auth: {
     user: EMAIL_ADDRESS,
     pass: EMAIL_PASSWORD,
